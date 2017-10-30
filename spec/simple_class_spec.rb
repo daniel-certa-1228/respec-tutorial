6972a79279f1fcdbbd1ac3a0014c2a2b08d88e3f
daniel-certa-1228 <mfdancerta@gmail.com>
2017-10-30 13:45:46 -0500

    Through before and after hooks

---
+++ spec/simple_class_spec.rb
@@ -0,0 +1,22 @@
+require 'simple_class'
+
+describe SimpleClass do
+
+  before(:each) do
+    @simple_class = SimpleClass.new
+  end
+
+  it "should have an initial message" do
+    expect(@simple_class).to_not be_nil
+    @simple_class.message = 'Something else...'
+  end
+
+  it "should be able to change its message" do
+    @simple_class.update_message('a new message')
+    expect(@simple_class).to_not be 'howdy'
+  end
+
+end
+
+
+
